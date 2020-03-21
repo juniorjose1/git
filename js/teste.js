c91195489fa1d14279c6de2cc8d64d6c1e11d32f
Alexandre <jajrjunior95@gmail.com>
2020-03-20 22:05:42 -0300

    Colocando sobrenome do Vitor

--- js/teste.js
+++ js/teste.js
@@ -12,7 +12,7 @@ var obj = {
 var obj2 = {
     "jogador1": "cristiano ronaldo Junior",
     "jogador2": "lionel messi",
-    "jogador3": "vitor"
+    "jogador3": "vitor ribeiro"
 }
 
 var array = [0, "Alexandre", true];
